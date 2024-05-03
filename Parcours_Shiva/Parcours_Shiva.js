206edeb0100ebc8440811a0172fb4b34fce498f7
Clément <clement33.moreaupro@gmail.com>
2024-05-04 00:53:44 +0200

    Parcours Shiva

--- Parcours_Shiva/Parcours_Shiva.js
+++ Parcours_Shiva/Parcours_Shiva.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  chargerParcours("15jours"); // Chargez les parcours de 15 jours par défaut
+  chargerParcours("15jours");
 });
 
 document.getElementById("btn15").addEventListener("click", function () {
@@ -14,39 +14,33 @@ document.getElementById("btn25").addEventListener("click", function () {
   chargerParcours("25jours");
 });
 
-function showLoading() {
-  document.getElementById("fauxChargement").style.display = "block";
-}
-
-function hideLoading() {
-  document.getElementById("fauxChargement").style.display = "none";
-}
+document.querySelectorAll("button").forEach(button => {
+  button.addEventListener("click", function(){
+    document.querySelectorAll("button").forEach(b => b.classList.remove("active-button"));
+    button.classList.add("active-button");
+  })
+})
 
 function chargerParcours(duree) {
   fetch(
-    "https://raw.githubusercontent.com/Clemom/Site_Bali/main/Parcours_Shiva_15/Parcours_Shiva.json"
+    "https://raw.githubusercontent.com/Clemom/Site_Bali/main/Parcours_Shiva/Parcours_Shiva.json"
   )
     .then((response) => response.json())
     .then((data) => {
-      let filteredParcours;
+      let filtreParcours;
       if (duree === "25jours") {
-        // Inclut les parcours de 15, 20 et 25 jours
-        filteredParcours = data.parcours.filter(
-          (p) =>
-            p.duree === "15jours" ||
-            p.duree === "20jours" ||
-            p.duree === "25jours"
+        filtreParcours = data.parcours.filter(
+          (parcours) =>
+            parcours.duree === "15jours" || parcours.duree === "20jours" || parcours.duree === "25jours"
         );
       } else if (duree === "20jours") {
-        // Inclut les parcours de 15 et 20 jours
-        filteredParcours = data.parcours.filter(
-          (p) => p.duree === "15jours" || p.duree === "20jours"
+        filtreParcours = data.parcours.filter(
+          (parcours) => parcours.duree === "15jours" || parcours.duree === "20jours"
         );
       } else {
-        // Seulement les parcours de 15 jours
-        filteredParcours = data.parcours.filter((p) => p.duree === "15jours");
+        filtreParcours = data.parcours.filter((parcours) => parcours.duree === "15jours");
       }
-      affichageParcours(filteredParcours);
+      affichageParcours(filtreParcours);
     })
     .catch((error) =>{
       console.error("Erreur lors du chargement des parcours:", error);
@@ -56,48 +50,46 @@ function chargerParcours(duree) {
 
 function affichageParcours(parcours) {
   const container = document.getElementById("parcoursContainer");
+  container.classList.add("hidden");
+  setTimeout(() =>{
   container.innerHTML = "";
   parcours.forEach((lieu) => {
-    const parcoursElem = document.createElement("div");
-    parcoursElem.className = "container-parcours1";
+    const parcoursElement = document.createElement("div");
+    parcoursElement.className = "container-parcours1";
 
-    const introElem = document.createElement("div");
-    introElem.className = "container-intro";
-    introElem.innerHTML = `<h1>${lieu.titre}</h1><p>Jours: ${lieu.jours}</p>`;
+    const introElement = document.createElement("div");
+    introElement.className = "container-intro";
+    introElement.innerHTML = `<h1>${lieu.titre}</h1><p>${lieu.jours}</p>`;
 
     const carteDecouvrirContainer = document.createElement("div");
     carteDecouvrirContainer.className = "container-carte-decouvrir";
 
-    lieu.activites.forEach((act) => {
+    lieu.activites.forEach((activites) => {
       const carteParcours = document.createElement("div");
       carteParcours.className = "carte-parcours";
       carteParcours.innerHTML = `
-                <img class="carte-image" src="${act.image}" alt="${act.jour}">
+                <img class="carte-image" src="${activites.image}" alt="${activites.jour}">
                 <div class="carte-contenu">
-                    <h4>${act.jour}</h4>
+                    <h4>${activites.jour}</h4>
                     <div class="carte-infos">
-                        <p>${act.description}</p>
+                        <p>${activites.description}</p>
                     </div>
                 </div>
             `;
       carteDecouvrirContainer.appendChild(carteParcours);
     });
 
-    parcoursElem.appendChild(introElem);
-    parcoursElem.appendChild(carteDecouvrirContainer);
+    parcoursElement.appendChild(introElement);
+    parcoursElement.appendChild(carteDecouvrirContainer);
 
     const boutonPlus = document.createElement("div");
     boutonPlus.className = "bouton-plus";
     boutonPlus.innerHTML = `<a href="#">En savoir plus</a>`;
 
-    parcoursElem.appendChild(boutonPlus);
-    container.appendChild(parcoursElem);
+    parcoursElement.appendChild(boutonPlus);
+    container.appendChild(parcoursElement);
   });
-  // Ajouter l'animation
-  setTimeout(() => {
+    container.classList.remove("hidden");
     container.classList.add("visible");
   }, 10);
-  setTimeout(() => {
-    container.classList.remove("visible");
-  }, 5555100); // La durée de la transition plus le temps que tu veux que le contenu reste visible
 }
